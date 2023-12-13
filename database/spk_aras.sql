433c6084e944e9f2225bb56dfa0ba4113868cf16
sinopaaalll <nrizqullah825@gmail.com>
2023-12-13 17:06:34 +0700

    fix database

--- database/spk_aras.sql
+++ database/spk_aras.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Dec 07, 2023 at 10:35 PM
+-- Generation Time: Dec 13, 2023 at 11:05 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 5.6.40
 
@@ -39,11 +39,16 @@ CREATE TABLE `alternatives` (
 --
 
 INSERT INTO `alternatives` (`id`, `name`, `description`) VALUES
-(6, 'A1', 'PT. A'),
-(7, 'A2', 'PT. B'),
-(8, 'A3', 'PT. C'),
-(9, 'A4', 'PT. D'),
-(10, 'A5', 'PT. E');
+(6, 'A1', 'Berkat Baja'),
+(7, 'A2', 'PD. Masa Baru'),
+(8, 'A3', 'Pt. Cahaya Sukses Pratama'),
+(9, 'A4', 'Pt. Oasis Surya sukses'),
+(10, 'A5', 'Cv. Sinar Surya sentosa'),
+(11, 'A6', 'Cv. Maju Jaya'),
+(12, 'A7', 'Pt. Adhi Cakra Utama Mulya'),
+(13, 'A8', 'Cv. Karya Guna Lestari'),
+(14, 'A9', 'Mega Baja'),
+(15, 'A10', 'Logam Tech');
 
 -- --------------------------------------------------------
 
@@ -64,11 +69,11 @@ CREATE TABLE `criteria` (
 --
 
 INSERT INTO `criteria` (`id`, `name`, `description`, `weight`, `jenis`) VALUES
-(7, 'C1', 'Kualitas Barang', 0.3, 'benefit'),
-(8, 'C2', 'Harga barang', 0.2, 'cost'),
-(9, 'C3', 'Waktu pengiriman barang', 0.2, 'cost'),
+(7, 'C1', 'Kualitas Barang', 0.2, 'benefit'),
+(8, 'C2', 'Harga barang', 0.3, 'cost'),
+(9, 'C3', 'Waktu pengiriman barang', 0.15, 'cost'),
 (10, 'C4', 'Banyaknya stok barang', 0.15, 'benefit'),
-(11, 'C5', 'Pelayanan', 0.15, 'benefit');
+(11, 'C5', 'Pelayanan', 0.2, 'benefit');
 
 -- --------------------------------------------------------
 
@@ -88,31 +93,56 @@ CREATE TABLE `evaluations` (
 --
 
 INSERT INTO `evaluations` (`id`, `criteria_id`, `alternative_id`, `value`) VALUES
-(9, 7, 6, 0.7),
-(10, 8, 6, 1),
-(11, 9, 6, 0.7),
-(12, 10, 6, 1),
+(9, 7, 6, 5),
+(10, 8, 6, 4),
+(11, 9, 6, 1),
+(12, 10, 6, 4),
 (13, 11, 6, 1),
-(14, 7, 7, 0.7),
-(15, 8, 7, 1),
-(16, 9, 7, 1),
-(17, 10, 7, 0.7),
-(18, 11, 7, 1),
-(19, 7, 8, 1),
-(20, 8, 8, 0.7),
-(21, 9, 8, 1),
-(22, 10, 8, 0.7),
-(23, 11, 8, 0.7),
-(24, 7, 9, 1),
-(25, 8, 9, 0.7),
-(26, 9, 9, 1),
-(27, 10, 9, 0.5),
-(28, 11, 9, 0.7),
-(29, 7, 10, 1),
-(30, 8, 10, 0.5),
-(31, 9, 10, 0.7),
-(32, 10, 10, 0.7),
-(33, 11, 10, 0.7);
+(14, 7, 7, 3),
+(15, 8, 7, 5),
+(16, 9, 7, 5),
+(17, 10, 7, 3),
+(18, 11, 7, 4),
+(19, 7, 8, 4),
+(20, 8, 8, 3),
+(21, 9, 8, 4),
+(22, 10, 8, 3),
+(23, 11, 8, 5),
+(24, 7, 9, 4),
+(25, 8, 9, 4),
+(26, 9, 9, 4),
+(27, 10, 9, 5),
+(28, 11, 9, 4),
+(29, 7, 10, 3),
+(30, 8, 10, 5),
+(31, 9, 10, 3),
+(32, 10, 10, 2),
+(33, 11, 10, 4),
+(34, 7, 11, 4),
+(35, 8, 11, 4),
+(36, 9, 11, 1),
+(37, 10, 11, 2),
+(38, 11, 11, 3),
+(39, 7, 12, 3),
+(40, 8, 12, 3),
+(41, 9, 12, 3),
+(42, 10, 12, 2),
+(43, 11, 12, 4),
+(44, 7, 13, 4),
+(45, 8, 13, 3),
+(46, 9, 13, 2),
+(47, 10, 13, 3),
+(48, 11, 13, 3),
+(49, 7, 14, 5),
+(50, 8, 14, 1),
+(51, 9, 14, 4),
+(52, 10, 14, 1),
+(53, 11, 14, 3),
+(54, 7, 15, 1),
+(55, 8, 15, 3),
+(56, 9, 15, 4),
+(57, 10, 15, 4),
+(58, 11, 15, 5);
 
 -- --------------------------------------------------------
 
@@ -132,11 +162,16 @@ CREATE TABLE `result` (
 --
 
 INSERT INTO `result` (`id`, `alternative_id`, `result`, `ranking`) VALUES
-(1, 6, '0.8057', 2),
-(2, 7, '0.6990', 3),
-(3, 8, '0.7857', 4),
-(4, 9, '0.7534', 5),
-(5, 10, '0.9079', 1);
+(1, 6, '0.5528', 2),
+(2, 7, '0.4004', 9),
+(3, 8, '0.5216', 4),
+(4, 9, '0.5118', 5),
+(5, 10, '0.3963', 10),
+(6, 11, '0.5289', 3),
+(7, 12, '0.4501', 8),
+(8, 13, '0.5049', 6),
+(9, 14, '0.7081', 1),
+(10, 15, '0.4599', 7);
 
 -- --------------------------------------------------------
 
@@ -157,21 +192,31 @@ CREATE TABLE `subcriteria` (
 --
 
 INSERT INTO `subcriteria` (`id`, `criteria_id`, `name`, `description`, `weight`) VALUES
-(11, 7, 'Barang tidak catat dan stok baru', '', 1),
-(12, 7, 'Barang catat dan stok baru', '', 0.7),
-(13, 7, 'Barang catat dan stok lama', '', 0.5),
-(14, 8, '<= Rp. 1.300.000', '', 1),
-(15, 8, 'Rp 1.310.000 – Rp 2.000.000', '', 0.7),
-(17, 8, '=> Rp 2.000.000', '', 0.5),
-(18, 9, '1-2 Hari', '', 1),
-(19, 9, '3-4 Hari', '', 0.7),
-(20, 9, '> 4 Hari', '', 0.5),
-(21, 10, 'Banyak', '', 1),
-(22, 10, 'Cukup', '', 0.7),
-(23, 10, 'Sedikit', '', 0.5),
-(24, 11, 'Sopan dan respon cepat', '', 1),
-(25, 11, 'Sopan dan respon lambat', '', 0.7),
-(26, 11, 'Tidak sopan dan respon lambat', '', 0.5);
+(11, 7, 'Selisih Dimensi >1 s/d <=0 mm', 'Sangat baik', 5),
+(12, 7, 'Selisih Dimensi >2 s/d <=1 mm', 'Baik', 4),
+(13, 7, 'Selisih Dimensi >3 s/d <=2 mm', 'cukup', 3),
+(14, 8, '>80 s/d <=100 Juta', '', 5),
+(15, 8, '>60 s/d <=80 Juta', '', 4),
+(17, 8, '>40 s/d <=60 Juta', '', 3),
+(18, 9, '1-3 Hari', '', 5),
+(19, 9, '3-6 Hari', '', 4),
+(20, 9, '6-9 Hari', '', 3),
+(21, 10, 'Sesuai Pesanan', '', 5),
+(22, 10, 'Kurang dari pesanan (minus 5 barang)', '', 4),
+(23, 10, 'Kurang dari pesanan (minus 10 barang)', '', 3),
+(24, 11, 'Respon sangat cepat', '', 5),
+(25, 11, 'Respon cepat', '', 4),
+(26, 11, 'Respon tidak cepat dan tidak lambat', '', 3),
+(27, 7, 'Selisih Dimensi >4 s/d <=3 mm', 'Buruk', 2),
+(28, 7, 'Selisih Dimensi >5 s/d <=4 mm', 'Sangat buruk', 1),
+(29, 8, '>20 s/d <=40 Juta', '', 2),
+(30, 8, '>10 s/d <=20 Juta', '', 1),
+(31, 9, '9-12 hari', '', 2),
+(32, 9, '> 9 hari', '', 1),
+(33, 10, 'Kurang dari pesanan (minus 15 barang)', '', 2),
+(34, 10, 'Kurang dari pesanan (minus 20 barang)', '', 1),
+(35, 11, 'Respon lambat', '', 2),
+(36, 11, 'Respon sangat lambat', '', 1);
 
 --
 -- Indexes for dumped tables
@@ -219,7 +264,7 @@ ALTER TABLE `subcriteria`
 -- AUTO_INCREMENT for table `alternatives`
 --
 ALTER TABLE `alternatives`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `criteria`
@@ -231,19 +276,19 @@ ALTER TABLE `criteria`
 -- AUTO_INCREMENT for table `evaluations`
 --
 ALTER TABLE `evaluations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- AUTO_INCREMENT for table `result`
 --
 ALTER TABLE `result`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `subcriteria`
 --
 ALTER TABLE `subcriteria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- Constraints for dumped tables
